2a3f5e820ca0dabdc772d02c06ab18e01c45787d
kotathefriends <5070kouta@gmail.com>
2024-01-16 16:49:25 +0900

    Handling user input の実装

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by KOTA TAKAHASHI on 2024/01/16.
+//
+
+import SwiftUI
+
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
+
